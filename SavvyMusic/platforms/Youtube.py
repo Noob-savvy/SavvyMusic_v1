05f80b8962065649174885f76c965a7f98d90cdd
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-01-21 17:35:21 +0530

    Update Youtube.py

--- SavvyMusic/platforms/Youtube.py
+++ SavvyMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from SavvyMusic.utils.database import is_on_off
+from SavvyMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
