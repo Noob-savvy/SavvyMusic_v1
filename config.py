b36aaa23d491a3ccd7f432c63c385f7ae633286c
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-01-31 20:15:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","SAVVY MUSIC")
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Noob-savvy/SavvyMusic_v1",
