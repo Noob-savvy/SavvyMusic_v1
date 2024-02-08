3c6da9dc19e764cbefb58263f6bf021e4333719a
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-02-09 01:38:53 +0530

    Update thumbnails.py

--- SavvyMusic/utils/thumbnails.py
+++ SavvyMusic/utils/thumbnails.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic import app
+from SavvyMusic import app
 from config import YOUTUBE_IMG_URL
 
 
@@ -71,8 +71,8 @@ async def get_thumb(videoid):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("AnonXMusic/assets/font.ttf", 30)
+        arial = ImageFont.truetype("SavvyMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("SavvyMusic/assets/font.ttf", 30)
         draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
             (55, 560),
