e35949eef2e7c873b9f3755ef93437312dae7c99
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-21 13:17:24 +0530

    Update bot.py

--- SavvyMusic/core/bot.py
+++ SavvyMusic/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class Savvy(Client):
+class savvy(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
